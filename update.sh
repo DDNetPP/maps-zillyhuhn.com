6418e0f5d1d08476e30c5ba97bdad3a6066c6382
ChillerDragon <ChillerDragon@gmail.com>
2022-09-16 17:52:27 +0200

    Skip done maps

--- update.sh
+++ update.sh
@@ -10,16 +10,24 @@ fi
 function hash_map() {
 	local map="$1"
 	local checksum
+	local outfile
 	if [ ! -f "$map" ]
 	then
 		echo "Error: map not found '$map'"
 		exit 1
 	fi
 	checksum="$(sha256sum "$map" | cut -d' ' -f1)"
-	mv "$map" "$(basename "$map" .map)_$checksum.map"
+	outfile="$(basename "$map" .map)_$checksum.map"
+	if [ "$outfile" ]
+	then
+		echo "[*] already got '$map'"
+		return
+	fi
+	echo "[*] adding '$map' ($outfile)"
+	mv "$map" "$outfile"
 }
 
-wget https://github.com/DDNetPP/maps/raw/master/BlmapChill.map 
+wget -q https://github.com/DDNetPP/maps/raw/master/BlmapChill.map 
 hash_map BlmapChill.map
 
 
